823ee9731fa8d03558b86604c3cff74c75079f6f
Ongon <49462670+LeeChangon@users.noreply.github.com>
2022-09-23 19:51:38 +0900

    [Silver II] Title: 상자넣기, Time: 128 ms, Memory: 115136 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+s = list(map(int, input().split()))
+dp = []
+dp.append(1)
+for i in range(1, n):
+    d = []
+    for j in range(i):
+        if s[i] > s[j]:
+            d.append(dp[j] + 1)
+    if not d:
+        dp.append(1)
+    else:
+        dp.append(max(d))
+print(max(dp))
