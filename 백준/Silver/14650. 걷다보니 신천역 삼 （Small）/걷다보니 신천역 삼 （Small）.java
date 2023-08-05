a551d39d13e72f4d75a5fe05a690b78e243380db
Ongon <49462670+LeeChangon@users.noreply.github.com>
2023-08-06 02:15:16 +0900

    [Silver II] Title: 걷다보니 신천역 삼 (Small), Time: 108 ms, Memory: 15696 KB -BaekjoonHub

---
+++ 백준/Silver/14650. 걷다보니 신천역 삼 （Small）/걷다보니 신천역 삼 （Small）.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	static int N;
+	static int cnt;
+	
+	static int addNum(int a, String num) {		//
+		return Integer.parseInt(Integer.toString(a) + num);
+	}
+	
+	static void recur(int depth, int a) {
+		if(depth == N) {
+			if(a % 3 == 0) cnt++;
+			return;
+		}
+		
+		
+		recur(depth + 1, addNum(a, "0"));
+		recur(depth + 1, addNum(a, "1"));
+		recur(depth + 1, addNum(a, "2"));
+	}
+    public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	N = Integer.parseInt(br.readLine());
+    	cnt = 0;
+    	
+    	recur(1, 1);
+    	recur(1, 2);
+    	
+    	System.out.println(cnt);
+    }
+}
